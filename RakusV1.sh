d1ee151a2febdf9a89f813dfab2da8a12d2a2bc3
fendikidang <50740208+fendikidang@users.noreply.github.com>
2019-07-22 17:25:35 +0700

    Add files via upload

---
+++ RakusV1.sh
@@ -0,0 +1,389 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            Sabar Cok :v"   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"           Sabar Cok :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"           Sabar Cok :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"           Sabar Cok :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            Nah Sudahkan,Selamat Mencoba"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+figlet -f slant "Mr.Rakus"|lolcat
+figlet -f slant "Fendi"|lolcat
+sleep 3
+echo
+echo $pur"==================="$cy" ============================="
+echo $i"Ôûê"$pu"  AUTHOR    :  Mr.Rakus  "$i"                    "
+echo $i"Ôûê"$pu"  WhatsApp  :  082338065465"$i"                "
+echo $i"Ôûê"$pu"  Instagram :  fendipercel__           "
+echo $i"Ôûê"$pu"  GMAIL     :  fendikidang65@gmail.com"$i"     "
+echo $i"Ôûê"$pu"  YOUTUBE   :  YouTube GHE Gammer banci,Hacker,Editor"$i"                 "
+echo $pur"==================="$cy" ============================="
+echo
+echo $cy"Menu Kidangv1 Tools:"
+echo $cy"Pilih Salah Satu Cok:"
+echo $ku"["$me"1"$ku"]"$i"Dark FB"
+echo $ku"["$me"2"$ku"]"$i"Tools Bajingan"
+echo $ku"["$me"3"$ku"]"$i"Lacak Lokasi"
+echo $ku"["$me"4"$ku"]"$i"Kumpulan Tools Hack FB"
+echo $ku"["$me"5"$ku"]"$i"WEBDAV"
+echo $ku"["$me"6"$ku"]"$i"Prankers"
+echo $ku"["$me"7"$ku"]"$i"Scan Kelemahan Web"
+echo $ku"["$me"8"$ku"]"$i"Scan Web (Part 2)"
+echo $ku"["$me"9"$ku"]"$i"Hack IG"
+echo $ku"["$me"10"$ku"]"$i"Webpwn"
+echo $ku"["$me"11"$ku"]"$i"Zanscan"
+echo $ku"["$me"12"$ku"]"$i"Wpscan"
+echo $ku"["$me"13"$ku"]"$i"SQLmap"
+echo $ku"["$me"14"$ku"]"$i"XssPy"
+echo $ku"["$me"15"$ku"]"$i"Hack IG Part 1"
+echo $ku"["$me"16"$ku"]"$i"Hack IG Part 2"
+echo $ku"["$me"17"$ku"]"$i"Hack IG Part 3"
+echo $ku"["$me"18"$ku"]"$i"Hack CCTV"
+echo $ku"["$me"19"$ku"]"$i"Hack WIFI"
+echo $i"["$me"20"$i"]"$ku"EXIT"
+echo
+echo $pur"Ôò¡ÔöÇ["$pur"Masukkan Nomer Cok"$pur"]"
+read -p"Ôò¦ÔöÇ>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 2
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 3
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 4 ] 
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 4
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+ls
+chmod +x new.sh
+sh new.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 5
+git clone https://github.com/ClayHackerTeam/webdav
+cd webdav
+sh webdav.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 6
+git clone https://github.com/ClayHackerTeam/Prankers
+cd Prankers
+ls
+sh Prankers.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 7
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 8
+git clone https://github.com/TechnicalMujeeb/TM-scanner
+cd TM-scanner
+chmod +x install.sh
+ls
+bash install.sh
+ls
+python2 tmscanner.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 9
+git clone https://github.com/senitopeng/instabot
+cd instabot
+pip2 install -r requirements.txt
+bash instabot
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 2
+git clone https://github.com/zigoo0/webpwn3r.git
+cd webpwn3r
+chmod +x *
+python2 scan.py
+fi
+
+if [ $pil = 11 ]
+then 
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 2
+git clone https://github.com/PhobiaXploit/Zanscan
+cd Zanscan
+chmod +x zanscan.sh
+bash zanscan.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+slepp 2
+git clone https://github.com/wpscanteam/wpscan/
+cd wpscan
+chmod 777 wpscan.rb
+gem install bundle
+bundle install -j5
+ruby wpscan.rb
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 2
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python sqlmap.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "Mr.Rakus"|lolcat
+sleep 2
+git clone https://github.com/faizann24/XssPy.git
+cd XssPy
+pip install mechanize
+python XssPy.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+echo $ku"          ++++TUNGGU SEBENTAR++++"
+toilet -f standard "Mr.Rakus" -F gay
+sleep 1
+git clone https://github.com/linuxkukeren/instagram
+cd instagram
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+python2 linuxkerenhackig.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+echo $ku"          ++++TUNGGU SEBENTAR++++"
+toilet -f standard "Mr.Rakus" -F gay
+sleep 1
+git clone https://github.com/DarkCurut08/DarkCurut08
+cd DarkCurut08 
+chmod +x Moreno77.sh
+sh Moreno77.sh
+fi
+
+if [ $pil = 17 ]
+then
+clear
+echo $ku"          ++++TUNGGU SEBENTAR++++"
+toilet -f standard "Mr.Rakus" -F gay
+sleep 1
+git clone https://github.com/thelinuxchoice/instainsane
+ls
+cd instainsane
+ls
+chmod +x *
+bash install.sh
+bash instainsane.sh
+fi
+
+if [ $pil = 18 ]
+then
+clear
+echo $ku"          ++++TUNGGU SEBENTAR++++"
+toilet -f standard "Mr.Rakus" -F gay
+sleep 1
+git clone https://github.com/kancotdiq/ipcs 
+cd ipcs
+python2 scan.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+echo $ku"          ++++TUNGGU SEBENTAR++++"
+toilet -f standard "Mr.Rakus" -F gay
+sleep 1
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+bash wifi-hacker.sh
+fi
+
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+figlet -f slant "MRX.pen"|lolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $i"====================================="
+sleep 2
+echo $ku"Instagram :"$i" fendipercel__"
+echo $ku"YouTube   :"$i" YouTube GHE Gammer banci,Hacker,Editor"
+echo $ku"WhatsApp  :"$i" 082338065465"
+sleep 2
+echo $i"====================================="
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+echo $ku"SUBSCRIBE MY CHANNEL { YOUTUBE GHE Gammer banci,Hacker,Editor }"
+exit
+fi
